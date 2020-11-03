4a1dfc9aaf511912a02a16816c1295276773e069
Kim yonggwan <sssw0101@naver.com>
2020-11-03 09:32:34 +0900

    Test2_Solution 수정
    중복제거 완료

--- CodingTest/src/Test2_Solution.java
+++ CodingTest/src/Test2_Solution.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class Test2_Solution {
     public static void main(String[] args) {
-        int[] arr = {2, 1, 3, 3};
-//        int[] arr = {3,4,4,2,5,2,5,5};
+//        int[] arr = {2, 1, 3, 3};
+        int[] arr = {3,4,4,2,5,2,5,5};
 //        int[] arr = {3,5,3,5,7,5,7};
         List<Integer> list = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>(); // 중복값 넣어둘 리스트
         Set<Integer> set = new HashSet<>();
-        int temp = 0;   // 임시 저장 값
 
-        for(int num : arr){ 
-            if (set.contains(num) || temp == num) { // set에 num의 값이 있거나 temp == num 이면 해당 값 제거
+        for(int num : arr){
+            if (set.contains(num) || tempList.contains(num)) { // set에 num의 값이 있거나 temp == num 이면 해당 값 제거
                 set.remove(num);
-                temp = num;
+                tempList.add(num);
                 continue;
             }else{
                 set.add(num);
