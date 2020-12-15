1d0a6dfa960d0d6c3f167b641d65d7069ab954b6
Kim yonggwan <sssw0101@naver.com>
2020-12-16 00:20:16 +0900

    12/16 SpringWeb 이메일 인증구현

--- SpringWeb/out/artifacts/SpringWeb_war_exploded/request.jsp
+++ SpringWeb/out/artifacts/SpringWeb_war_exploded/request.jsp
@@ -8,7 +8,7 @@
 <h2>폼 요청</h2>
 <form action="board/request.do" method="post">
     <%-- name 부여시 중요사항 VO와 맞출것 --%>
-    아이디 : <input type="text" name="id"><br/>
+    아이디 : <input type="email" name="id"><br/>
     이름 : <input type="text" name="name"><br/>
     나이 : <input type="number" name="age"><br/>
     <input type="submit" value="전송">
--- SpringWeb/src/main/java/controller/RequestController.java
+++ SpringWeb/src/main/java/controller/RequestController.java
@@ -1,16 +1,61 @@
 package controller;
 
+import model.MemberService;
 import model.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/board")
 public class RequestController {
 
+    @Autowired
+    JavaMailSender mailSender;
+    MemberService memberService;
+
     @RequestMapping("/request.do")
-    public String request(MemberVO memberVO){
+    public String request(MemberVO memberVO, HttpServletResponse response) throws IOException {
         System.out.println("/request.do 요청");
+
+        // 난수 생성 알아볼것
+        int random = new Random().nextInt(4589362) + 49311; // 난수
+
+        System.out.println("RequestController 30Line Random : " + random);
+        System.out.println("이메일 : " + memberVO.getId());
+
+        String sender = "sssw0101@gmail.com";
+        String receiver = memberVO.getId();
+        String title = "이메일 인증";
+        String content = "메일 확인 인증번호는 " + random + "입니다.";
+
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message,true,"utf-8");
+
+            messageHelper.setFrom(sender); // 보내는사람 생략하면 정상작동을 안함
+            messageHelper.setTo(receiver); // 받는사람 이메일
+            messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+            messageHelper.setText(content); // 메일 내용
+
+            mailSender.send(message);
+            System.out.println("메일 전송 완료");
+        }catch (Exception e){
+            System.out.println("이메일 전송 에러  : " + e.toString());
+        }
+
+        response.setContentType("text/html; charset=utf-8");
+        PrintWriter out_email = response.getWriter();
+        out_email.flush();
+
         return "board/request";
     }
 
